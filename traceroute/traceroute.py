b759189e94395d8705f790fe6d4dba888aef5673
lucan8 <gamer.lucan72@gmail.com>
2025-05-23 22:51:34 +0300

    Emptying icmp recv queue until hitting valid respond. Using both hardcoded ips and dns resolved one.  Code refactor

--- traceroute/traceroute.py
+++ traceroute/traceroute.py
@@ -3,13 +3,25 @@ import requests
 import folium
 from pathlib import Path
 import random
+from typing import Dict, List, Tuple
 
 # IT SEEMS THAT OFTEN TRACEROUTE DOES NOT REACH THE DESTINATION
-# TODO: Eliminate unrelated packets(put them in a special queue and print)
-# TODO: CHOOSE BETTER SITES
+# HARDCODING IPS FOR SITES THOUGH RESOLVING THEM WITH DNS WOULD BE MORE INTERESTING
+# TODO(EXTRA): Put unrelated packets in a special queue and print
+# TODO(EXTRA): Send multiple packets for the same TTL 
+# TODO(EXTRA): Get all ip addresses of a site and test them(use ipv6 as well)
 
-# Check for the existence of region and also use regionName instead of region
+def getLocalIp():
+    dummy_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    dummy_sock.connect(('8.8.8.8', 0))
+
+    ip = dummy_sock.getsockname()[0]
+    dummy_sock.close()
+
+    return ip
+
 class IPLocation:
+    MY_LOCAL_IP = getLocalIp()
     def __init__(self, json):
         self.ip = json.get('query')
         self.country = json.get('country', 'N/A')
@@ -17,19 +29,12 @@ class IPLocation:
         self.city = json.get('city', 'N/A')
         self.lat = json.get('lat')
         self.lon = json.get('lon')
+
     def getLocationStr(self):
         return f"{self.country} - {self.region} - {self.city}"
-    
-# socket de UDP
-udp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-# socket RAW de citire a răspunsurilor ICMP
-icmp_recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-# setam timout in cazul in care socketul ICMP la apelul recvfrom nu primeste nimic in buffer
-icmp_recv_socket.settimeout(5)
 
-# Get machine ip
-MY_LOCAL_IP = socket.gethostbyname(socket.gethostname())
-def _traceroute(ip, port, TTL):
+
+def _traceroute(ip, port, TTL, udp_send_sock, icmp_recv_sock):
     print("Current TTL: ", TTL)
     # setam TTL in headerul de IP pentru socketul de UDP
     udp_send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, TTL)
@@ -39,26 +44,32 @@ def _traceroute(ip, port, TTL):
 
     addr = None
     try:
-        data, addr = icmp_recv_socket.recvfrom(63535)
+        while True:
+            data, addr = icmp_recv_sock.recvfrom(63535)
 
-        # First 4 bits give the size of the IP header in 32-bit words
-        ip_header_length = (data[0] & 0x0f) * 4
+            # First 4 bits give the size of the IP header in 32-bit words
+            ip_header_length = (data[0] & 0x0f) * 4
 
-        # Extract source and destination of the icmp packet for unexpected behaviour
-        src_ip = socket.inet_ntoa(data[12:16])
-        dest_ip = socket.inet_ntoa(data[16:20])
+            # Extract source and destination of the icmp packet for unexpected behaviour
+            src_ip = socket.inet_ntoa(data[12:16])
+            dest_ip = socket.inet_ntoa(data[16:20])
 
-        print(f"SOURCE: {src_ip}, DESTINATION: {dest_ip}")
-        print(f"ICMP RESP TYPE: {data[ip_header_length]}, CODE: {data[ip_header_length + 1]}")
-        
-        # data[ip_header_length] = Type, data[ip_header_length + 1] = Code
-        if data[ip_header_length] != 11:
-            if data[ip_header_length + 1] != 3:
-                addr = None
-                print(f"Destination host unreachable!")
-        elif data[ip_header_length + 1] != 0:
+            print(f"SOURCE: {src_ip}, DESTINATION: {dest_ip}")
+            print(f"ICMP RESP TYPE: {data[ip_header_length]}, CODE: {data[ip_header_length + 1]}")
+
+            # TTL exceeded
+            if data[ip_header_length] == 11 and data[ip_header_length + 1] == 0:
+                break
+            
+            # Port unreachable (we reached destination)
+            if data[ip_header_length] == 3 and data[ip_header_length + 1] == 3:
+                break
+            
+            if data[ip_header_length] == 3 and data[ip_header_length + 1] == 1:
+                print("Host unreachable!")
+
+            # Other types and codes are garbage
             addr = None
-            print("Fragment reassembly time exceeded!")
         
     except socket.timeout as e:
         print('    *   ' * 3)
@@ -66,19 +77,32 @@ def _traceroute(ip, port, TTL):
 
 
 def traceroute(ip, port):
+    # Set ttl range
     ttl = 1
     MAX_TTL = 30
 
     print("Running for: ", ip, port)
+
+    # Will containt collected addresses
     addresses = []
 
+    # Create udp socket for sending data
+    udp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+    
+    # Create raw icmp socket for responses
+    icmp_recv_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    icmp_recv_sock.settimeout(5)
+    
     # Get all addresses from traceroute until either MAX_TTL or destination is reached
     while not addresses or (addresses[-1] != ip and ttl < MAX_TTL):
-        addr = _traceroute(ip, port, ttl)
+        addr = _traceroute(ip, port, ttl, udp_send_sock, icmp_recv_sock)
         if addr is not None:
             addresses.append(addr[0])
             print(addresses[-1])
         ttl += 1
+
+    udp_send_sock.close()
+    icmp_recv_sock.close()
 
     return addresses
 
@@ -177,50 +201,85 @@ def plot_route_on_map(locations: list[IPLocation], output_html_file: str):
     except Exception as e:
         print(f"Error saving map: {e}")
 
+# Creates the files and directories neccessary to represent
+# The path traversed by packets to reach the ips of the given sites of the given regions as a string and as a map
+# The hardcoded flag specifies whether to use the ips given or to solve them using dns
+def solve(region_sites_ips: Dict[str, List[Tuple[str, str]]], hardcoded_ips: bool = True):
+    # Chooses start directory based on flag
+    start_dir = ""
+    if hardcoded_ips:
+        start_dir = "docs/hardcoded_ips"
+    else:
+        start_dir = "docs/dns_solved_ips"
+
+    # Create start directory
+    Path(start_dir).mkdir(parents=True, exist_ok=True)
+
+    print(f"MY LOCAL IP: {IPLocation.MY_LOCAL_IP}")
+    out_file = open(f'{start_dir}/traceroute.md', 'w')
+    out_file.write(f'# Traceroute paths starting from {IPLocation.MY_LOCAL_IP}\n\n')
+    out_file.write('Note: The map does not perfectly represent the geolocation of the machines\n\n')
+
+    # Create directory which will hold the visual representation of the paths per region
+    maps_dir = 'maps'
+    Path(f'{start_dir}/{maps_dir}').mkdir(exist_ok=True)
+
+    for reg, domains in region_sites_ips.items():
+        out_file.write("## " + reg + '\n\n')
+        Path(f'{start_dir}/{maps_dir}/{reg}').mkdir(exist_ok=True)
+
+        # For each domain get associated ip, traceoute path and it's corresponding map
+        for i, site in enumerate(domains):
+            ip = site[1]
+            # Using dns to get ip if not hardcoded
+            if not hardcoded_ips:
+                # If using dns does not work, fallback on hardcoded ip
+                try:
+                    ip = socket.gethostbyname(site[0])
+                except:
+                    print(f"WARNING: Could not get ip for {site[0]}")
+
+            out_file.write(f"{i + 1}. Site: {site[0]}({ip})\n\n")
+            out_file.write(f"   - Path: \n")
+            locations = getRouteLocations(ip)
+
+            printRouteLocations(out_file, locations)
+            plot_route_on_map(locations, f"{start_dir}/{maps_dir}/{reg}/out_map_{i + 1}.html")
+            out_file.write(f"   - [Visual map representations]({start_dir}/{maps_dir}/{reg}/out_map_{i + 1}.html)\n\n")
+
 def main():
-    region_sites = {
-        "Africa": [
-            ("www.uct.ac.za", "137.158.154.230"),
-            ("www.unilag.edu.ng", "196.45.48.5"),
-            ("www.au.int", "196.1.95.34"),
-            ("www.kemri.go.ke", "41.204.161.195"),
-            ("www.moroccoworldnews.com", "172.67.72.169")
-        ],
+    region_sites_ips = {
         "Asia": [
-            ("www.tokyo-u.ac.jp", "133.11.0.23"),
-            ("www.iitm.ac.in", "14.139.160.3"),
-            ("www.ntu.edu.sg", "155.69.7.49"),
-            ("www.korea.ac.kr", "163.152.6.10"),
-            ("www.cuhk.edu.hk", "137.189.97.31")
+            ("www.iisc.ac.in", "14.139.196.22"),
+            ("www.ntu.edu.tw", "140.112.1.1"),
+            ("www.kaist.ac.kr", "143.248.1.140"),
+            ("www.chula.ac.th", "161.246.70.53"),
+            ("www.vnu.edu.vn", "119.17.215.11")
+        ],
+        "Africa": [
+            ("www.uct.ac.za", "137.158.154.1"),
+            ("www.kenet.or.ke", "196.200.219.1"),
+            ("www.cu.edu.eg", "163.121.128.11"),
+            ("www.unilag.edu.ng", "41.203.64.1"),
+            ("www.um5.ac.ma", "195.221.1.1")
         ],
         "Australia": [
-            ("www.abc.net.au", "203.2.218.214"),
-            ("www.anu.edu.au", "150.203.2.53"),
-            ("www.sydney.edu.au", "129.78.5.8"),
-            ("www.csiro.au", "138.194.190.20"),
-            ("www.australia.gov.au", "152.91.62.30")
+            ("homer.unimelb.edu.au", "128.250.21.21"),
+            ("rsise.anu.edu.au", "150.203.179.10"),
+            ("www.uq.edu.au", "130.102.6.1"),
+            ("ftp.griffith.edu.au", "131.181.2.10"),
+            ("www.service.tas.gov.au", "103.26.158.22")
+        ],
+        "Europe": [
+            ("www.dfn.de", "188.1.144.1"),
+            ("www.cern.ch", "188.184.116.1"),
+            ("www.ras.ru", "84.237.1.1"),
+            ("www.uv.es", "158.42.1.1"),
+            ("www.uj.edu.pl", "149.156.1.1")
         ]
     }
 
-    print(f"MY LOCAL IP: {MY_LOCAL_IP}")
-    out_file = open('traceroute.md', 'w')
-    out_file.write(f'# Traceroute paths starting from {MY_LOCAL_IP}\n\n')
-    out_file.write('Note: The map does not perfectly represent the geolocation of the machines\n\n')
-    
-    maps_dir = 'maps'
-    Path(maps_dir).mkdir(exist_ok=True)
-
-    for reg, sites in region_sites.items():
-        out_file.write("## " + reg + '\n\n')
-        Path(f'{maps_dir}/{reg}').mkdir(exist_ok=True)
-
-        for i, site in enumerate(sites):
-            out_file.write(f"{i + 1}. Site: {site[0]}({site[1]})\n\n")
-            out_file.write(f"   - Path: \n")
-            locations = getRouteLocations(site[1])
-
-            printRouteLocations(out_file, locations)
-            plot_route_on_map(locations, f"{maps_dir}/{reg}/out_map_{i + 1}.html")
-            out_file.write(f"   - [Visual map representations]({maps_dir}/{reg}/out_map_{i + 1}.html)\n\n")
+    solve(region_sites_ips, False)
+    solve(region_sites_ips)
 
 main()
