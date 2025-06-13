0a6e9017afcc71330d25c29050b4698de75be5a6
lucan8 <gamer.lucan72@gmail.com>
2025-06-13 22:55:48 +0300

    File renameing and added tcp hijacking and arp spoofing

--- cristi/src/tcp_server.py
+++ cristi/src/tcp_server.py
@@ -8,18 +8,22 @@ logging.basicConfig(format = u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
 
 port = 10000
-adresa = 'localhost'
+name = "server"
+adresa = '198.7.0.2'
 server_address = (adresa, port)
 sock.bind(server_address)
 logging.info("Serverul a pornit pe %s si portnul portul %d", adresa, port)
 sock.listen(5)
+logging.info('Asteptam conexiui...')
+conexiune, address = sock.accept()
+logging.info("Handshake cu %s", address)
+
 while True:
-    logging.info('Asteptam conexiui...')
-    conexiune, address = sock.accept()
-    logging.info("Handshake cu %s", address)
-    time.sleep(2)
-    data = conexiune.recv(1024)
-    logging.info('Content primit: "%s"', data)
-    conexiune.send(b"Server a primit mesajul: " + data)
-    conexiune.close()
-sock.close()
+    try:
+        data = conexiune.recv(1024)
+        logging.info('Content primit: "%s"', data)
+        conexiune.send(b"Server a primit mesajul: " + data)
+    except:
+        sock.close()
+        logging.info("Server is closing")
+        break
---
+++ cristi/traceroute/docs/hardcoded_ips/traceroute.md
@@ -0,0 +1,13 @@
+# Traceroute paths starting from 164.92.244.69
+
+Note: The map does not perfectly represent the geolocation of the machines
+
+## Europe
+
+1. Site: twitch.tv(151.101.66.167)
+
+   - Path: 
+Canada - Ontario - Toronto -> United States - New York - New York -> Canada - Quebec - Montreal
+
+   - [Visual map representations](/traceroute/docs/hardcoded_ips/maps/Europe/out_map_1.html)
+
