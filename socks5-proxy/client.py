a495cf8abe6d84d08cbd3a91578e15d215b56bdc
lucan8 <gamer.lucan72@gmail.com>
2025-07-30 16:11:56 +0300

    SOCKS5: Forwarding messages work both ways now. Browser still does not show anything. Added helper functions for sending and receiving DNS and http packets

--- socks5-proxy/client.py
+++ socks5-proxy/client.py
@@ -3,17 +3,40 @@ import threading
 import sys
 from scapy.all import DNS, DNSQR
 import base64
+import hashlib
+import traceback
+from typing import Union
+
+class RemoteConn:
+    def __init__(self, rem_sock: socket.socket, client_rem_sock_id: int = 0):
+        self.rem_sock = rem_sock
+        # self.rem_sock.settimeout(10)
+        self.client_rem_sock_id = client_rem_sock_id
+        self.buffer = ""
+
+    def add_to_buffer(self, message: str):
+        self.buffer += message
+    
+    # Send the data in the buffer and empties it
+    def send(self):
+        decoded_http_req = myB64Decode(self.buffer)
+        # print(decoded_http_req.decode(errors='ignore'))
+        self.rem_sock.sendall(decoded_http_req)
+        self.buffer = ""
+    
+    def __del__(self):
+        self.rem_sock.close()
 
 # Useful links:
 # Built in types: https://docs.python.org/3/library/stdtypes.html
 # Base64: https://docs.python.org/3/library/base64.html
 # Socket: https://docs.python.org/3/library/socket.html
+# Scapy: https://scapy.readthedocs.io/en/latest/api/scapy.html
 
 # TODO:
-# DO THE NUMBER ENCODING RIGHT ON THE SERVER
 # Optimization: Strip the padding when sending a message, add it back on the receiving side beofre decoding
+# Optimization: Determine the remaining TXT size dynamically
 # Good practice: Encode and decode all data
-# Construct function that adds BASE_DOMAIN to every DNS request
 # After each packet sent, wait for a request for md5 and send it
 # If they don't match resend the packet
 # Also have a timeout of 2-3 sec, if no response comes back resend the packet
@@ -26,16 +49,19 @@ DNS_SERVER_IP = '34.159.249.73'
 DNS_SERVER_PORT = 53
 BASE_DOMAIN = "vilgax.crabdance.com"
 dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-dns_socket.settimeout(10)
+dns_socket.settimeout(100)
+DNS_PACKET_ID = 0xAAAA
 
 SOCK_SERVER_PORT = 1080
 SOCK_SERVER_IP = '127.0.0.1'
 
 # Operations that will be done by the dns server
 CREATE_CONN = 1 # Query: cmd.port.addr
-CONSTRUCT_REQ = 2 # Query: cmd.remote_sock_id.encoded_http_req
-SEND_REQ = 3 # Query: cmd.remote_sock_id
+CONSTRUCT_MSG = 2 # Query: cmd.remote_sock_id.encoded_http_req
+SEND_MSG = 3 # Query: cmd.remote_sock_id
 
+MAX_HTTP_SIZE = 4096
+MAX_UDP_SIZE = 512
 CHUNK_SIZE = 200
 MAX_LABEL_SIZE = 60 # Make sure it's multiple of 4
 
@@ -43,7 +69,17 @@ MAX_LABEL_SIZE = 60 # Make sure it's multiple of 4
 DUMMY_IP = '0.0.0.1'
 DUMMY_PORT = 9999
 
-def proc_dns_resp(dns_resp: DNS) -> bytes:
+# rem_sock_id - RemoteConn
+conn_dict = {}
+rem_sock_id_counter = 1
+
+def myB64Decode(data: str):
+    needed_pad = 4 - (len(data) % 4)
+    data += '=' * needed_pad
+    return base64.b64decode(data)
+
+# Extracts the TXT section of the answer section and decodes if desired
+def proc_dns_resp(dns_resp: DNS, decode=False):
     rr = dns_resp.an[0]
 
     # TXT record is encoded as 16
@@ -51,55 +87,129 @@ def proc_dns_resp(dns_resp: DNS) -> bytes:
         raise RuntimeError("Not TXT record")
 
     txt_data = rr.rdata
+    if not decode:
+        return txt_data
+    
     txt_data = b''.join(txt_data).decode()
-    txt_data = base64.b64decode(txt_data)
+    txt_data = myB64Decode(txt_data)
 
     return txt_data
 
-def forward_to_dns(client_sock: socket.socket, req_addr: str, req_port: int):
-    global BASE_DOMAIN, dns_socket
+# Tells the DNS server to create a connection to req_addr, req_port
+# And to add rem_sock_id_counter to all responses
+def create_conn(rem_sock: socket.socket, req_addr: str, req_port: int) -> RemoteConn:
+    global rem_sock_id_counter
 
-    # Create initial connection from dns server to (req_addr, req_port)
-    domain = f'{CREATE_CONN}.{req_port}.{req_addr}.{BASE_DOMAIN}'
-    dns_query = DNS(id=0xAAAA, rd=1, qd=DNSQR(qname=domain, qtype="TXT"))
-    dns_socket.sendto(bytes(dns_query), (DNS_SERVER_IP, DNS_SERVER_PORT))
+    send_dns_req(f'{CREATE_CONN}.{rem_sock_id_counter}.{req_port}.{req_addr}')
+    conn_dict[rem_sock_id_counter] = RemoteConn(rem_sock)
+    rem_sock_id_counter += 1
 
-    # Wait for remote_sock id response
-    rem_sock_id = int.from_bytes(proc_dns_resp(DNS(dns_socket.recv(512))))
-    print(f"[+] Remote socket id: {rem_sock_id}")
+    return conn_dict[rem_sock_id_counter - 1]
 
-    # Get request from browser and forward it to dns server, encoded
-    http_req = client_sock.recv(4096)
-    print(f"[+] Received http request of size {len(http_req)}")
-    # print(http_req.decode(errors='ignore'))
+# Adds ".{BASE_DOMAIN}" to domain and sends the request
+def send_dns_req(domain: str):
+    global dns_socket, DNS_SERVER_IP, DNS_SERVER_PORT, DNS_PACKET_ID
 
-    http_req_b32 = base64.b64encode(http_req).decode('ascii')
-    rem_http_req = http_req_b32
+    domain += f".{BASE_DOMAIN}"
+    dns_query = bytes(DNS(id=DNS_PACKET_ID, rd=1, qd=DNSQR(qname=domain, qtype="TXT")))
+    dns_socket.sendto(dns_query, (DNS_SERVER_IP, DNS_SERVER_PORT))
+        
+# Encodes http req in base64 and sends it to the DNS server in chunks
+def send_http_msg(rem_conn: RemoteConn, http_msg: bytes):
+    global CONSTRUCT_MSG, MAX_LABEL_SIZE, MAX_HTTP_SIZE
+    
+    # Encode http request from browser
+    http_msg_b32 = base64.b64encode(http_msg).decode('ascii')
+    rem_http_msg = http_msg_b32
     chunk_index = 0
     
-    # Send request in chunks, also specifying which socket to use
-    while len(rem_http_req):
-        chunk = rem_http_req[:MAX_LABEL_SIZE]
-        # print(f"[+] Sending chunk {chunk_index} of the http request")
-        # print(base64.b64decode(chunk).decode(errors='ignore'))
-        domain = f'{CONSTRUCT_REQ}.{rem_sock_id}.{chunk}.{BASE_DOMAIN}'
+    # Send request in chunks, also specifying which socket the server should use
+    while len(rem_http_msg):
+        chunk = rem_http_msg[:MAX_LABEL_SIZE]
+        send_dns_req(f'{CONSTRUCT_MSG}.{rem_conn.client_rem_sock_id}.{chunk}')
 
-        dns_query = DNS(id=0xAAAA, rd=1, qd=DNSQR(qname=domain, qtype="TXT"))
-        dns_socket.sendto(bytes(dns_query), (DNS_SERVER_IP, DNS_SERVER_PORT))
+        print(f"[+] HTTP chunk {chunk_index} sent")
+        rem_http_msg = rem_http_msg[MAX_LABEL_SIZE:]
 
         chunk_index += 1
-        rem_http_req = rem_http_req[MAX_LABEL_SIZE:]
     
     if chunk_index != 0:
         # Tell the server to forward the completed message
-        print(f"[+] HTTP request completely sent")
-        domain = f'{SEND_REQ}.{rem_sock_id}.{BASE_DOMAIN}'
+        print(f"[+] HTTP message completely sent")
+        send_dns_req(f'{SEND_MSG}.{rem_conn.client_rem_sock_id}')
 
-        dns_query = DNS(id=0xAAAA, rd=1, qd=DNSQR(qname=domain, qtype="TXT"))
-        dns_socket.sendto(bytes(dns_query), (DNS_SERVER_IP, DNS_SERVER_PORT))
 
+def handle_dns_resp(data: bytes):
+    global BASE_DOM_NAME_COUNT, conn_dict, rem_sock_id_counter, dns_socket
+    dns_packet = DNS(data)
+
+    # Only accept response packets
+    if dns_packet.qr == 1:
+        query_name = dns_packet[DNSQR].qname.decode().rstrip('.')
+        label_parts = query_name.split('.')
+
+        print(f"[+] Query name: {query_name}")
+
+        if len(label_parts) < 3:
+            print("[ERROR] Invalid query")
+            return
+
+        cmd, rem_sock_id = int(label_parts[0]), int(label_parts[1])
+          
+        if rem_sock_id not in conn_dict:
+            print("[ERROR] Unknown remote socket id")
+            return
+    
+        chosen_rem_sock = conn_dict[rem_sock_id]
+
+        if cmd == CREATE_CONN:
+            client_rem_sock_id = int.from_bytes(proc_dns_resp(dns_packet, True))
+            chosen_rem_sock.client_rem_sock_id = client_rem_sock_id
+
+            print("[+] Connection to remote succesful")
+            forward_to_dns(chosen_rem_sock)
+        elif cmd == CONSTRUCT_MSG:
+            encoded_msg_l = proc_dns_resp(dns_packet)
+            chunk_size = 0
+            # print(base64.b64decode(encoded_msg).decode(errors='ignore'))
+            # Add partial encoded request to buffer
+            for enc_msg in encoded_msg_l:
+                msg = enc_msg.decode("ascii")
+                # print(msg)
+                chosen_rem_sock.add_to_buffer(msg)
+                chunk_size += len(enc_msg)
+
+            print(f"[+] Socket {rem_sock_id}: Adding {chunk_size}")
+        elif cmd == SEND_MSG:
+            # Decode http request and send to the browser
+            chosen_rem_sock.send()
+            print(f"[+] Socket {rem_sock_id}: Sent HTTP request")
+        else:
+            print(f"[ERROR] Invalid command: {cmd}")
+    else:
+        print("[ERROR] Invalid DNS packet")
+
+def forward_to_dns(rem_conn: RemoteConn):
+    global dns_socket
+
+    while True:
+        data = rem_conn.rem_sock.recv(MAX_HTTP_SIZE)
+        if not data:
+            break
+        send_http_msg(rem_conn, data)
+
+def forward_from_dns():
+    global dns_socket
+
+    while True:
+        data = dns_socket.recv(MAX_UDP_SIZE)
+        if not data:
+            break
+        handle_dns_resp(data)
 
 def handle_client(sock: socket.socket):
+    global rem_sock_id_counter, SOCKS_VERSION, DUMMY_IP, DUMMY_PORT
+
     vers, nmethods = sock.recv(2)
     assert vers == SOCKS_VERSION
 
@@ -135,12 +245,17 @@ def handle_client(sock: socket.socket):
         sock.sendall(bytes([SOCKS_VERSION, 0, 0, 1]) + 
                      socket.inet_pton(socket.AF_INET, DUMMY_IP) +
                      DUMMY_PORT.to_bytes(2, 'big'))
-        forward_to_dns(sock, addr, port)
+        
+       
+        create_conn(sock, addr, port)
     except Exception as e:
-        print(f"ERROR: Establishing connection: {e}")
+        print(f"ERROR: Establishing connection: {traceback.format_exc()}")
         sock.close()
 
+print(f"[+] Listening for DNS responses from {(DNS_SERVER_IP, DNS_SERVER_PORT)}")
+threading.Thread(target=forward_from_dns).start()
 
+# Socket for new TCP conncection with browser
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((SOCK_SERVER_IP, SOCK_SERVER_PORT))
 sock.listen(5)
@@ -151,10 +266,8 @@ print(f"[+] SOCKS5 server listeing on interface: {(SOCK_SERVER_IP, SOCK_SERVER_P
 # dns_socket.sendto(b"hello", (DNS_SERVER_IP, DNS_SERVER_PORT))
 # forward_to_dns(None, DNS_SERVER_IP, DNS_SERVER_PORT)
 
-while True:
-    serv_sock, _ = sock.accept()
-    print("[+] Accepted a new connection")
+for i in range(3):
+    serv_sock, addr = sock.accept()
+    print(f"[+] Accepted a new connection: {addr}")
     # threading.Thread(target=handle_client, args=(serv_sock,)).start()
     handle_client(serv_sock)
-    # break # Only for now
-
--- socks5-proxy/server.py
+++ socks5-proxy/server.py
@@ -2,13 +2,15 @@ import socket
 import threading
 from scapy.all import DNS, DNSQR, DNSRR
 import base64
+from typing import Union
+import traceback
 
 # TODO: Use a new thread for each message receive
-    
-class RemoteSock:
-    def __init__(self, rem_sock: socket.socket):
+class RemoteConn:
+    def __init__(self, rem_sock: socket.socket, client_rem_sock_id: int):
         self.rem_sock = rem_sock
-        self.rem_sock.settimeout(5)
+        # self.rem_sock.settimeout(5)
+        self.client_rem_sock_id = client_rem_sock_id
         self.buffer = ""
 
     def add_to_buffer(self, message: str):
@@ -16,20 +18,28 @@ class RemoteSock:
     
     # Send the data in the buffer and empties it
     def send(self):
-        decoded_http_req = base64.b64decode(self.buffer)
+        decoded_http_req = myB64Decode(self.buffer)
         # print(decoded_http_req.decode(errors='ignore'))
         self.rem_sock.sendall(decoded_http_req)
         self.buffer = ""
     
+def myB64Decode(data: str):
+    needed_pad = 4 - (len(data) % 4)
+    data += '=' * needed_pad
+    return base64.b64decode(data)
 
 BASE_DOMAIN = "vilgax.crabdance.com"
 BASE_DOM_NAME_COUNT = len(BASE_DOMAIN.split('.'))
 DNS_SERVER_PORT = 53
 DNS_SERVER_IP = '0.0.0.0'
 MESSAGE_SIZE = 200
+DNS_PACKET_ID = 0xAAAA
+DNS_QTYPE = "TXT"
+MAX_HTTP_SIZE = 1024
+MAX_UDP_SIZE = 512
 
-# id - connection
-rem_sock_dict = {}
+# remote_sock_id - RemoteConn
+conn_dict = {}
 rem_sock_id_counter = 1
 
 TEST_ADDR = ("www.google.com", 443)
@@ -40,12 +50,22 @@ CREATE_CONN = 1 # Query: cmd.port.addr
 CONSTRUCT_MSG = 2 # Query: cmd.remote_sock_id.encoded_http_req
 SEND_MSG = 3 # Query: cmd.remote_sock_id
 
-def forward_from_proxy():
-    ...
-def forward_to_proxy():
-    ...
+def send_dns_resp(dns_req: DNS, client_addr: tuple[bytes, int], msg: bytes):
+    global dns_socket
+    dns_socket.sendto(bytes(build_dns_resp(dns_req, msg)), client_addr)
 
-def build_dns_resp(dns_req: DNS, msg: bytes) -> DNS:
+def send_fake_dns_resp(domain: str, client_addr: tuple[bytes, int], msg: bytes):
+    global dns_socket
+    dns_socket.sendto(bytes(build_fake_dns_resp(domain, msg)), client_addr)
+
+# Actually responds to dns_req
+# If msg is str, the function assumes it is already encoded in base64, otherwise it encodes it
+def build_dns_resp(dns_req: DNS, msg: Union[bytes, str]) -> DNS:
+    global DNS_QTYPE
+    
+    if isinstance(msg, bytes):
+        msg = base64.b64encode(msg).decode()
+
     return DNS(
                 id=dns_req.id,
                 qr=1,
@@ -53,15 +73,79 @@ def build_dns_resp(dns_req: DNS, msg: bytes) -> DNS:
                 qd=dns_req.qd,
                 an=DNSRR(
                     rrname=dns_req.qd.qname,
-                    type="TXT",
+                    type=DNS_QTYPE,
                     ttl=60,
-                    rdata= base64.b64encode(msg).decode()
+                    rdata=msg
                 )
             )
 
+# Mimics a dns response to the "domain" query
+# If msg is str, the function assumes it is already encoded in base64, otherwise it encodes it
+def build_fake_dns_resp(domain: str, msg: Union[bytes, str]) -> DNS:
+    global DNS_PACKET_ID, DNS_QTYPE
+
+    domain += f".{BASE_DOMAIN}"
+    if isinstance(msg, bytes):
+        msg = base64.b64encode(msg).decode()
+
+    return DNS(
+                id=DNS_PACKET_ID,
+                qr=1,
+                aa=1,
+                qd=DNSQR(qname=domain, qtype=DNS_QTYPE),
+                an=DNSRR(
+                    rrname=domain,
+                    type=DNS_QTYPE,
+                    ttl=60,
+                    rdata=msg
+                )
+            ) 
+        
+def forward_to_dns(rem_conn: RemoteConn, client_addr: tuple[bytes, int]):
+    global dns_socket
+
+    while True:
+        data = rem_conn.rem_sock.recv(MAX_HTTP_SIZE)
+        if not data:
+            break
+        print("[+] Received HTTP response from remote! Forwarding...")
+        send_http_resp(rem_conn, client_addr, data)
+
+def forward_from_dns():
+    global dns_socket
+
+    while True:
+        data, client_addr = dns_socket.recvfrom(MAX_UDP_SIZE)
+        if not data:
+            break
+        handle_dns_query(data, client_addr)
+
+def send_http_resp(rem_conn: RemoteConn, client_addr: tuple[bytes, int], http_msg: bytes):
+    global CONSTRUCT_MSG, MESSAGE_SIZE, MAX_HTTP_SIZE
+    
+    # Encode http request from browser
+    http_msg_b32 = base64.b64encode(http_msg).decode('ascii')
+    # print(http_msg.decode('utf-8', errors='ignore'))
+    rem_http_msg = http_msg_b32
+    chunk_index = 0
+    
+    # Send request in chunks, also specifying which socket the server should use
+    while len(rem_http_msg):
+        chunk = rem_http_msg[:MESSAGE_SIZE]
+        send_fake_dns_resp(f'{CONSTRUCT_MSG}.{rem_conn.client_rem_sock_id}', client_addr, chunk)
+
+        print(f"[+] HTTP chunk {chunk_index} sent")
+        rem_http_msg = rem_http_msg[MESSAGE_SIZE:]
+
+        chunk_index += 1
+    
+    if chunk_index != 0:
+        # Tell the server to forward the completed message
+        print(f"[+] HTTP message completely sent")
+        send_fake_dns_resp(f'{SEND_MSG}.{rem_conn.client_rem_sock_id}', client_addr, '')
 
 def handle_dns_query(data: bytes, client_addr: tuple[bytes, int]):
-    global BASE_DOM_NAME_COUNT, rem_sock_dict, rem_sock_id_counter, dns_socket
+    global BASE_DOM_NAME_COUNT, conn_dict, rem_sock_id_counter, dns_socket
     dns_packet = DNS(data)
 
     if dns_packet.qr == 0 and dns_packet.qd:
@@ -79,9 +163,9 @@ def handle_dns_query(data: bytes, client_addr: tuple[bytes, int]):
         if cmd == CREATE_CONN:
             # Quick fix until dns server is set up
             if "vilgax" in query_name:
-                req_port, req_addr = int(label_parts[1]), '.'.join(label_parts[2:len(label_parts) - 3])
+                client_rem_sock_id, req_port, req_addr = int(label_parts[1]), int(label_parts[2]), '.'.join(label_parts[3:len(label_parts) - 3])
             else:
-                req_port, req_addr = int(label_parts[1]), '.'.join(label_parts[2:])
+                client_rem_sock_id, req_port, req_addr = int(label_parts[1]), int(label_parts[2]), '.'.join(label_parts[3:])
 
             # Connect to the one the client wants to connect
             print(f"[+] Attempting connection to {req_addr}:{req_port}")
@@ -92,61 +176,51 @@ def handle_dns_query(data: bytes, client_addr: tuple[bytes, int]):
 
             # Add connection to the dictionary
             # TODO: Group this operation in a locked function
-            rem_sock_dict[rem_sock_id_counter] = RemoteSock(remote_sock)
+            conn_dict[rem_sock_id_counter] = RemoteConn(remote_sock, client_rem_sock_id)
             rem_sock_id_counter += 1
 
             print(f"[+] Socket {rem_sock_id_counter - 1}: Created")
 
             # Send the remote sock id back to the client
-            dns_socket.sendto(bytes(build_dns_resp(dns_packet, (rem_sock_id_counter - 1).to_bytes(4, 'big'))), client_addr)
+            send_dns_resp(dns_packet, client_addr, (rem_sock_id_counter - 1).to_bytes(4, 'big'))
+
+            # Keep conversation going between remote and DNS socket
+            threading.Thread(target=forward_to_dns, args=(conn_dict[rem_sock_id_counter - 1], client_addr,)).start()
         elif cmd == CONSTRUCT_MSG:
             rem_sock_id, encoded_msg = int(label_parts[1]), label_parts[2]
             
-            if rem_sock_id not in rem_sock_dict:
-                print("[ERROR] FORWARD before CREATE_CONN")
+            if rem_sock_id not in conn_dict:
+                print("[ERROR] CONSTRUCT_MSG before CREATE_CONN")
                 return
             
             # print(base64.b64decode(encoded_msg).decode(errors='ignore'))
             # Add partial encoded request to buffer
-            chosen_rem_sock = rem_sock_dict[rem_sock_id]
+            chosen_rem_sock = conn_dict[rem_sock_id]
             chosen_rem_sock.add_to_buffer(encoded_msg)
 
             print(f"[+] Socket {rem_sock_id}: Adding {len(encoded_msg)}")
         elif cmd == SEND_MSG:
             rem_sock_id = int(label_parts[1])
             
-            if rem_sock_id not in rem_sock_dict:
-                print("[ERROR] SEND_REQ before CREATE_CONN")
+            if rem_sock_id not in conn_dict:
+                print("[ERROR] SEND_MSG before CREATE_CONN")
                 return
             
             # Decode http request and send to the browser
-            chosen_rem_sock = rem_sock_dict[rem_sock_id]
+            chosen_rem_sock = conn_dict[rem_sock_id]
             chosen_rem_sock.send()
             print(f"[+] Socket {rem_sock_id}: Sent HTTP request")
-
-            # Receive response from remote
-            http_resp = chosen_rem_sock.rem_sock.recv(4096)
-            print(f"[+] Socket {rem_sock_id}: Received http response")
-
-            # print(http_resp.decode(errors='ignore'))
         else:
-            print("[ERROR] Invalid command")
-        
+            print(f"[ERROR] Invalid command: {cmd}")
+    else:
+        print("[ERROR] Invalid DNS packet")
+
+print(f"Listening for DNS queries on interface ({DNS_SERVER_IP}):{DNS_SERVER_PORT}...")    
 dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dns_socket.bind((DNS_SERVER_IP, DNS_SERVER_PORT))
-print(f"Listening for DNS queries on interface ({DNS_SERVER_IP}):{DNS_SERVER_PORT}...")
+forward_from_dns()
 
 # Test connection
 print(f"[+] Testing connenction to {TEST_ADDR}")
 socket.create_connection(TEST_ADDR)
 print(f"[+] Connection successful")
-
-while True:
-    data, client_addr = dns_socket.recvfrom(MESSAGE_SIZE)
-    try:
-        print(f"[+] Received request from {client_addr}")
-        handle_dns_query(data, client_addr)
-    except Exception as e:
-        print(f"[ERROR]: Handling req from {client_addr}, data: {data}: {e}")
-
-dns_socket.close()
